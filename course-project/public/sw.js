df017e00c3b890aa53629f5ebc48042187af2a6f
Leonardo Batista de Araujo <lbaraujo2@stefanini.com>
2019-11-20 11:07:35 -0300

    Non-Lifecycle Events

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function (event) {
     console.log('[ServiceWorker] - Activating Service Worker...', event);
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', function (event) {
+    console.log('[ServiceWorker] - Fetching something...', event);
+    event.respondWith(fetch(event.request));
+});
