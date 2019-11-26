51d51f0401ba0e0f9032357742d404a497d946aa
Leocaliban <leocaliban@gmail.com>
2019-11-25 23:41:09 -0300

    Add cache versioning variable

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v01';
+var CACHE_DYNAMIC_NAME = 'dynamic-v01';
+
 self.addEventListener('install', function (event) {
     console.log('[ServiceWorker] - Instaling Service Worker...', event);
     event.waitUntil(
-        caches.open('static-v2')
+        caches.open(CACHE_STATIC_NAME)
             .then(function (cache) {
                 console.log('[SERVICE WORKER] Precaching App Shell.');
                 cache.addAll([
@@ -30,7 +33,7 @@ self.addEventListener('activate', function (event) {
             .then(function (keyList) {
                 return Promise.all(
                     keyList.map(function (key) {
-                        if (key !== 'static-v2' && key !== 'dynamic') {
+                        if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
                             console.log('[ServiceWorker] - Removing old cache...', key);
                             return caches.delete(key);
                         }
@@ -49,7 +52,7 @@ self.addEventListener('fetch', function (event) {
                 } else {
                     return fetch(event.request)
                         .then(function (res) {
-                            return caches.open('dynamic')
+                            return caches.open(CACHE_DYNAMIC_NAME)
                                 .then(function (cache) {
                                     cache.put(event.request.url, res.clone());
                                     return res;
